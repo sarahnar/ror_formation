a66b0cb43096c37f1cc8e16b2c8a5cdbe57493a0
sarah <clergentsarah@gmail.com>
2017-08-09 23:18:33 -0400

    finish crud

--- app/controllers/concerns/posts_controller.rb
+++ app/controllers/concerns/posts_controller.rb
@@ -5,4 +5,43 @@ class PostsController < ApplicationController
 
     end
 
+    def show 
+        @post = Post.find(params[:id])
+    end
+    
+
+    def edit 
+        @post = Post.find(params[:id])
+    end
+
+
+    def update 
+        @post = Post.find(params[:id])
+        
+        post_params = params.require(:post).permit(:name, :content).inspect
+        @post.update(post_params)
+        redirect_to posts_path
+    end
+
+    def new
+
+        @post = Post.new
+    end
+    
+    def create 
+        Post = Post.create(post_params)
+        redirect_to post_path(post.id)
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        redirect_to posts_path
+    end
+    
+    private 
+
+    def post_params
+        params.require(:post).permit(:name, :content)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
 
+  resources :users
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/update'
+
+  get 'categories/destroy'
+
+  get 'categories/new'
+
+  get 'categories/edit'
+
   root to: 'pages#home'
   
   get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
 
-  ressources :posts
-
+  resources :posts
+  resources :categories
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
